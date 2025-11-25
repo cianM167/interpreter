fa0ab0d07aface672d7813d1987fe701d2ab419e
cianM167 <cianmcguire167@gmail.com>
2025-11-25 20:26:37 +0000

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,10 +1,5 @@
-use std::fs;
 use std::env;
 use std::fs::read_to_string;
-use std::mem::replace;
-use std::result;
-use regex::Regex;
-use std::alloc::{alloc, dealloc, Layout};
 
 //create variable struct
 struct Variable<T> {
@@ -25,18 +20,34 @@ fn read_lines(filename: &str) -> Vec<String> {
         .collect()  // gather them together into a vector
 }
 
-fn perform_operation(operString: String) -> u16{
-    if operString.contains("+") {
-        let cleanedString = operString.replace(" ", "");
-        let mut chunks = cleanedString.split("+");
-        let val1: u16 = chunks.next().unwrap().parse().unwrap();
-        let val2: u16 = chunks.next().unwrap().parse().unwrap();
+fn perform_operation(oper_string: String, mut var_vec: Vec<VariableTypes>, var_index: usize) {
+    let ref dir: &VariableTypes  = &var_vec[1];
+    match dir {
+        VariableTypes::Integer(ref mut &&variable) => {
+                if oper_string.contains("+") {
+                    let cleanedstring = oper_string.replace(" ", "");
+                    let mut chunks = cleanedstring.split("+");
+                    let val1: u32 = chunks.next().unwrap().parse().unwrap();
+                    let val2: u32 = chunks.next().unwrap().parse().unwrap();
+                    variable.val = Some(val1 + val2);
+                }
+            }
+            VariableTypes::Float(variable) => {
+                println!("Not implemented")
+            }
+    }
+
+
+    if oper_string.contains("+") {
+        let cleanedstring = oper_string.replace(" ", "");
+        let mut chunks = cleanedstring.split("+");
+        let val1: u32 = chunks.next().unwrap().parse().unwrap();
+        let val2: u32 = chunks.next().unwrap().parse().unwrap();
 
         println!("{}", val1 + val2);
-        return val1 + val2;
     } else {
         println!("Invalid operator");
-        return 0;
+        return;
     }
 }
 
@@ -105,14 +116,13 @@ fn main() {
 
     let file_vec = read_lines(&(args[1]));
     let mut var_vec: Vec<VariableTypes> = vec![];
-    let mut line_number = 0;
-    for line in file_vec {
-        line_number += 1;
+    for (line_number, line) in file_vec.iter().enumerate() {
         if !line.is_empty() {
             let start = (line.split(" ")).next().unwrap();
+            let (i, in_vec) = is_in_vec_tup(&var_vec, start.into());
 
             if line.starts_with("print") {
-                let mut arg = line.split_once("(").unwrap();
+                let arg = line.split_once("(").unwrap();
                 print(arg.1, &var_vec);
 
             } else if start == "let" {
@@ -145,8 +155,17 @@ fn main() {
                     //println!("Testing find func{}", is_in_vec(&var_vec, "a".into()))
                 }
 
-            //} else if is_in_vec(&var_vec, start.to_string()) {  
-                
+            } else if in_vec { 
+                if line.contains("=") {
+                    let arg = line.split_once("=").unwrap();
+                    if !arg.1.contains("=") {
+                        //println!("after equal:{}", arg.1);
+                        perform_operation(arg.1.into(), &mut var_vec, i);// variable is mutated by function
+                    } else {
+                        println!("incorrect assignment only one = allowed");
+                        return;
+                    }
+                }
             } else if line.starts_with("//") {
     
             } else {
