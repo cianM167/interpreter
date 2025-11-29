fad90e6ed495662051f88f63a52a53b64ef71463
cianM167 <cianmcguire167@gmail.com>
2025-11-29 13:51:55 +0000

    Add files via upload

--- parser/main.rs
+++ parser/main.rs
@@ -2,43 +2,66 @@ use regex::Regex;
 
 fn parse(string: &str) -> String {
     println!("in string:{}", string);
-    let mut res: String = "".into();
+    let mut res: String = string.to_string().replace(" ", "");
     let re = Regex::new(r"\((.*?)\)").unwrap();//very naiive
+    let mut out: String = " ".to_string();
 
-    for mat in re.find_iter(string) {
-        let eq = &string[mat.start()..mat.end()];
-        println!("eq:{}", eq);
+    let mut matches = tokenize(&res);
+
+    for mat in matches {
+        if mat.contains("(") {
+            out = parse(&mat);
+        } else {
+            out = mat.into();
+        }
+    }
+
+    /*
+    while !mat.is_empty() {
+        let matched_portion = mat[0];
+        let eq = &string[matched_portion.start()..matched_portion.end()];
         let mut parsed = rem_first_and_last(eq).to_owned();
+        println!("{}", parsed);
+
         if parsed.contains("(") {
             parsed = parsed + ")";
-            println!("{}", parsed);
             let to_add = parse(&parsed);
-            println!("{}", to_add);
-            parsed = re.replace(&parsed, to_add).to_string();
-  
-        }
+            res = re.replace(&res, to_add).to_string();
+            let mut open_bracket = false;
 
-            println!("res:{}", parsed);
+            for mut c in res.chars() {
+                if c == '(' {
+                    open_bracket = true;
+                } else if c == ')' && open_bracket {
+                    open_bracket = false;
+                } else if c == ')' {
+                    //c = '';
+                }
+            }
+        } else {
             parsed = parsed.replace(" ", "");
-
+            println!("parsed:{}", parsed);
             if parsed.contains("+") {
                 let chunks = parsed.split_once("+").unwrap();
-                println!("chunk:{}",chunks.0);
-                let num1: u32 = (chunks.0).parse().unwrap();
-                let num2: u32 = (chunks.1).parse().unwrap();
-                
-                parsed = (num1 + num2).to_string();
-                res = re.replace(&parsed, &parsed).to_string();
-                println!("sum:{}", res);
-            } else {
-                println!("error");
+
+                println!("chunk 0:{}", chunks.0);
+                let var1: u32 = chunks.0.parse().unwrap();
+                println!("chunk 1:{}", chunks.1);
+                let var2: u32 = chunks.1.parse().unwrap();
+
+                parsed = (var1 + var2).to_string();
             }
+
+            res = re.replace(&res, parsed).to_string();
+        }
+
+        println!("res:{}", res);
+        mat = re.find(&res); 
     }
+    */
     
-
-
-    println!("returning:{}", res);
-    return res;
+    println!("returning:{}", out);
+    return out;
 }
 
 fn rem_first_and_last(value: &str) -> &str {
@@ -48,7 +71,38 @@ fn rem_first_and_last(value: &str) -> &str {
     chars.as_str()
 }
 
+fn tokenize(value: &str) -> Vec<&str> {
+    let mut depth = 0;
+    let mut tok_start = 0;
+    let mut tok_end = 0;
+    let mut token_vector: Vec<&str> = vec![];
+    for (i,c) in value.chars().enumerate() {
+        if c == '(' {
+            if depth == 0 {
+                tok_start = i + 1;
+            }
+            depth += 1;
+        } else if c == ')' {
+            depth -= 1;
+            if depth == 0 {
+                tok_end = i;
+
+                token_vector.push(&value[tok_start..tok_end]);
+                println!("{:?}", token_vector);
+            } 
+
+        }
+    }
+
+    return token_vector;
+}
+
+
+
 fn main() {
-    let input = "(1 + 10) + (10 + (10))";
-    parse(input);
+    let input = "(10 + (21)) + 10";
+    let out = parse(input);
+    //println!("{:?}", tokenize(input));
+
+    //println!("{}", out);
 }
