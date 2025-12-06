9b78ad09cc6bbfa653368349b38261229758113e
cianM167 <cianmcguire167@gmail.com>
2025-12-06 17:35:10 +0000

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,264 +1,321 @@
-use std::fmt;
-use phf::phf_map;
+use std::env;
+use std::fs::read_to_string;
 
-#[derive(Debug, Clone)]
-enum TokenType {
-    //single character tokens
-    LeftParen, RightParen, LeftBrace, RightBrace,
-    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
-
-    //One or two character tokens
-    Bang, BangEqual,
-    Equal, EqualEqual,
-    Greater, GreaterEqual,
-    Less, LessEqual,
-
-    //Literals
-    Identifier(String), String(String), Integer(i32), Float(f32),
-
-    //Keywords
-    And, Struct, Else, False, Fun, For, If, Nil, Or,
-    Print, Return, True, Let, While,
-
-    Eof
+//create variable struct
+enum VariableType {
+    Integer(i32),
+    Float(f32),
+    String(String),
+    Bool(bool),
 }
 
-impl TokenType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-       write!(f, "{:?}", self)
+struct Variable {
+    name: String,
+    val: VariableType,
+}
+
+impl Variable {
+    fn mutate(&mut self, value: VariableType) {
+        self.val = value;
     }
 }
 
-enum ScanResult {
-    Success,
-    EndLine,
-    Error(String),
+fn read_lines(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()  // panic on possible file-reading errors
+        .lines()  // split the string into an iterator of string slices
+        .map(String::from)  // make each slice into a string
+        .collect()  // gather them together into a vector
 }
 
-enum StringResult {
-    Unterminated,
-    Found(String),
-}
+fn parse(string: &str) -> String {
+    //println!("in string:{}", string);
+    let mut res: String = string.to_string().replace(" ", "");
+    let mut out: String;
 
-enum ScanTokensResult {
-    Success,
-    Error(String),
-}
+    let matches = tokenize(&res);
 
-static Keywords: phf::Map<&'static str, TokenType> = phf_map! {//constant hashmap for reserved words
-    "and" => TokenType::And,
-    "else" => TokenType::Else,
-    "false" => TokenType::False,
-    "for" => TokenType::For,
-    "fun" => TokenType::Fun,
-    "if" => TokenType::If,
-    "nil" => TokenType::Nil,
-    "or" => TokenType::Or,
-    "print" => TokenType::Print,
-    "return" => TokenType::Return,
-    "true" => TokenType::True,
-    "let" => TokenType::Let,
-    "while" => TokenType::While,
-    "struct" => TokenType::Struct,
-};
-
-fn scan_token(it: &mut impl Iterator<Item = char>, vec: &mut Vec<TokenType>) -> ScanResult {
-    let c: char;
-    match it.next() {
-        None => return ScanResult::EndLine,
-        Some(next) => c = next,
-    }
-    match c {
-        '(' => vec.push(TokenType::LeftParen),
-        ')' => vec.push(TokenType::RightParen),
-        '{' => vec.push(TokenType::LeftBrace),
-        '}' => vec.push(TokenType::RightBrace),
-        ',' => vec.push(TokenType::Comma),
-        '.' => vec.push(TokenType::Dot),
-        '-' => vec.push(TokenType::Minus),
-        '+' => vec.push(TokenType::Plus),
-        ';' => vec.push(TokenType::Semicolon),
-        '*' => vec.push(TokenType::Star),
-        '!' => {
-            if matches(it, '=') {
-                vec.push(TokenType::BangEqual);
-            } else {
-                vec.push(TokenType::Bang);
-            };
-        }
-        '=' => {
-            if matches(it, '=') {
-                vec.push(TokenType::EqualEqual);
-            } else {
-                vec.push(TokenType::Equal);
-            };
-        }
-        '<' => {
-            if matches(it, '=') {
-                vec.push(TokenType::LessEqual);
-            } else {
-                vec.push(TokenType::Less);
-            };
-        }
-        '>' => {
-            if matches(it, '=') {
-                vec.push(TokenType::GreaterEqual);
-            } else {
-                vec.push(TokenType::Greater);
-            };
-        }
-        '/' => {
-            if matches(it, '/') {
-                return ScanResult::EndLine;
-            } else {
-                vec.push(TokenType::Slash);
-            }
-        }
-        ' ' => return ScanResult::Success,
-        '\r' => return ScanResult::Success,
-        '\t' => return ScanResult::Success,
-        '\n' => return ScanResult::EndLine,
-        '"' => {
-            match string(it) {
-                StringResult::Unterminated => return ScanResult::Error("Unterminated string".to_string()),
-                StringResult::Found(string) => {
-                    vec.push(TokenType::String(string));
-                }
-            };
-        }
-
-        default => {
-            println!("{}", default);
-            if (default.is_digit(10)) {
-                let number = number(c, it);
-                if number.contains(".") {
-                    vec.push(TokenType::Float(number.parse().unwrap()));//add case for unwrap failing
-                } else {
-                    vec.push(TokenType::Integer(number.parse().unwrap()));
-                }
-
-            } else if default.is_alphanumeric() {
-                let identifier = Identifier(default, it);
-                match Keywords.get(&identifier) {
-                    Some(keyword) => {
-                        vec.push(keyword.clone());//there might be a better way to do this
-                    }
-                    _ => vec.push(TokenType::Identifier(identifier)),
-                }
-            } else {
-               return ScanResult::Error("Character not recognised".to_string()) 
-            };
-        }
+    for mat in matches {
+        out = parse(&mat);
+        let tok_pos = first_token(&res);
+        res = res.replace(tok_pos, &out);
+        //println!("new string{}", res);
     }
 
-    return ScanResult::Success;
-}
+    if res.contains("<") {
+        let parts = res.split_once("<").unwrap();
 
-fn matches(it: &mut impl Iterator<Item = char>, expected: char) -> bool {
-    let mut peek_iter = it.peekable();
+        let val1 = parse(parts.0);
+        let val2 = parse(parts.1);
 
-    match peek_iter.peek() {
-        None => return false,
-        Some(next) => {
-            if *next == expected {
-                peek_iter.next();
-                return true;
-            } else {
-                return false;
-            }
-        }
+        res = (val1 > val2).to_string();
     }
-}
 
-fn string(it: &mut impl Iterator<Item = char>) -> StringResult {
-    let mut string = "".to_string();
-    loop {
-        match it.next() {
-            None => return StringResult::Unterminated,
-            Some(next) => {
-                if next == '"' {
-                    return StringResult::Found(string);
-                } else {
-                    string.push(next);
-                }
-            }
-        }
+    if res.contains(">") {
+        let parts = res.split_once(">").unwrap();
+
+        let val1 = parse(parts.0);
+        let val2 = parse(parts.1);
+
+        res = (val1 < val2).to_string();
+    }
+
+    if res.contains("==") {
+        let parts = res.split_once("==").unwrap();
+
+        let val1 = parse(parts.0);
+        let val2 = parse(parts.1);
+
+        res = (val1 == val2).to_string();
+    }
+
+    if res.contains("-") {
+        let parts = res.split_once("-").unwrap();
+
+        let val1: f32 = parse(parts.0).parse().unwrap();
+        let val2: f32 = parse(parts.1).parse().unwrap();
+
+        res = (val1 - val2).to_string();
+    }
+
+    if res.contains("+") {
+        let parts = res.split_once("+").unwrap();
+
+        let val1: f32 = parse(parts.0).parse().unwrap();
+        let val2: f32 = parse(parts.1).parse().unwrap();
+
+        res = (val1 + val2).to_string();
+    }
+
+    if res.contains("/") {
+        let parts = res.split_once("/").unwrap();
+
+        let val1: f32 = parts.0.parse().unwrap();
+        let val2: f32 = parts.1.parse().unwrap();
+
+        res = (val1 / val2).to_string();
+    }
+
+    if res.contains("*") {
+        let parts = res.split_once("*").unwrap();
+
+        let val1: f32 = parts.0.parse().unwrap();
+        let val2: f32 = parts.1.parse().unwrap();
+
+        res = (val1 * val2).to_string();
     }
     
+    //println!("returning:{}", res);
+    return res;
 }
 
-fn number(number_start: char, it: &mut impl Iterator<Item = char>) -> String {
-    let mut peek_iter = it.peekable();
-    let mut number: String = "".into();
-    number += &number_start.to_string();
+fn tokenize(value: &str) -> Vec<String> { //returns tokens inside "()"
+    let mut depth = 0;
+    let mut tok_start = 0;
+    let mut tok_end;
+    let mut token_vector: Vec<String> = vec![];
+    for (i,c) in value.chars().enumerate() {
+        if c == '(' {
+            if depth == 0 {
+                tok_start = i + 1;
+            }
+            depth += 1;
+        } else if c == ')' {
+            depth -= 1;
+            if depth == 0 {
+                tok_end = i;
 
-    loop {
-        println!("Number:{}", number);
-        match peek_iter.peek() {
-            None => return number,
-            Some(peeked) => {
-                if peeked.is_digit(10) || *peeked == '.' {
-                    let next = peek_iter.next().unwrap();
-                    number += &next.to_string();
-                } else {
-                    return number;
+                token_vector.push(value[tok_start..tok_end].to_string());
+                //println!("{:?}", token_vector);
+            } 
+
+        }
+    }
+
+    return token_vector;
+}
+
+fn first_token(value: &str) -> &str { //returns substring
+    let mut depth = 0;
+    let mut tok_start = 0;
+    let tok_end;
+
+    for (i,c) in value.chars().enumerate() {
+        if c == '(' {
+            if depth == 0 {
+                tok_start = i;
+            }
+            depth += 1;
+        } else if c == ')' {
+            depth -= 1;
+            if depth == 0 {
+                tok_end = i + 1;
+
+                return &value[tok_start..tok_end];
+            } 
+
+        }
+    }
+
+    return ""; //error out
+}
+
+fn is_in_vec(vec: &Vec<Variable>, varname: String) -> bool {
+    for variable in vec {
+        if variable.name == varname {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn is_in_vec_tup(vec: &Vec<Variable>, varname: String) -> (usize, bool) {
+    for (i, variable) in vec.iter().enumerate() {
+        if variable.name == varname {
+            return (i, true);
+        }
+    }
+    return (0, false);
+}
+
+fn replace_varname_in_string(value: &str, var_vec: &Vec<Variable>) -> String {
+    let mut string = value.to_string();
+    for variable in  var_vec {
+
+        match &variable.val {
+            VariableType::Integer(variable_type) => {
+                if value.contains(&variable.name) {
+                    string = string.replace(&variable.name, &variable_type.to_string());
+                }
+            }
+            VariableType::Float(variable_type) => {
+                if value.contains(&variable.name) {
+                    string = string.replace(&variable.name, &variable_type.to_string());
+                }
+            }
+            VariableType::Bool(variable_type) => {
+                if value.contains(&variable.name) {
+                    string = string.replace(&variable.name, &variable_type.to_string());
+                }
+            }
+            VariableType::String(variable_type) => {
+                if value.contains(&variable.name) {
+                    string = string.replace(&variable.name, &variable_type.to_string());
                 }
             }
         }
     }
+
+    return string;
 }
 
-fn scan_tokens(str: String) -> ScanTokensResult {
-    let mut tokens:Vec<TokenType> = vec![];
+fn print(p_string: &str, var_vec: &Vec<Variable>) {
+    let arguments_string: String = tokenize(p_string)[0].to_string();
+    let arguments = arguments_string.split(",");
 
-    
-    let mut iter = str.chars().into_iter().peekable();
-    let mut end  = false;
-    while !end {
-        match scan_token(&mut iter, &mut tokens) {
-            ScanResult::EndLine => {
-                end = true;
-            },
-            ScanResult::Error(message) => { 
-                return ScanTokensResult::Error(message)//add line number to error
-            },
-            ScanResult::Success => end = false,
+    for argument in arguments {
+        if argument.contains('"') {
+            let to_print = &(argument.replace(&['"'], ""));
+            print!("{}",to_print);
+        } else {
+            let to_print = parse(&replace_varname_in_string(&argument, &var_vec));
+            
+            print!("{}", to_print);
+            
         }
+
     }
 
-    println!("{:?}", tokens);
-
-    return ScanTokensResult::Success;
-    
+    print!("\n");
 }
-
-fn Identifier(identifier_start: char, it: &mut impl Iterator<Item = char>) -> String {
-    let mut peek_iter = it.peekable();
-    let mut identifier: String = "".into();
-    identifier += &identifier_start.to_string();
-
-    loop {
-        println!("Identifier:{}", identifier);
-        match peek_iter.peek() {
-            None => return identifier,
-            Some(peeked) => {
-                if peeked.is_alphanumeric() || *peeked == '.' {
-                    let next = peek_iter.next().unwrap();
-                    identifier += &next.to_string();
-                } else {
-                    return identifier;
-                }
-            }
-        }
-    }
-}
-
-fn lexer() {
-
-}
-
-
 
 fn main() {
-    scan_tokens("let a".into());
+    let args: Vec<String> = env::args().collect(); 
+    //println!("{:?}",args);
+    //let variable_name = fs::read_to_string(args[1].clone()).unwrap();
+
+    let file_vec = read_lines(&(args[1]));
+    let mut var_vec: Vec<Variable> = vec![];
+    for (line_number, line) in file_vec.iter().enumerate() {
+        if !line.is_empty() {
+            let start = (line.split(" ")).next().unwrap();
+            let (i, in_vec) = is_in_vec_tup(&var_vec, start.into());
+
+            if line.starts_with("print") {
+                let arg = line.split_once("print").unwrap();
+                print(arg.1, &var_vec);
+
+            } else if start == "let" {
+                let arg = line.split_once(" ").unwrap();
+                let assignment = &arg.1.replace(" ", "");
+                let parts = assignment.split_once("=").unwrap();
+
+                let variable_name = parts.0;
+                let variable_value_string = parse(&replace_varname_in_string(parts.1, &var_vec));
+                let variable_value: VariableType;
+
+                if variable_value_string.contains(".") {
+                    variable_value = VariableType::Float(variable_value_string.parse().unwrap());
+                } else if variable_value_string == "true" || variable_value_string == "false" {
+                    variable_value =
+                        if variable_value_string == "true" {
+                            VariableType::Bool(true)
+                        } else {
+                            VariableType::Bool(false)
+                        };
+                } else {
+                    variable_value = VariableType::Integer(variable_value_string.parse().unwrap());
+                }
+
+                let new_variable: Variable = Variable { 
+                    name: variable_name.to_string(),
+                     val: variable_value,
+                };
+
+                var_vec.push(new_variable);
+
+            } else if line.starts_with("if") {
+                let arg = line.split_once("if").unwrap();
+                let mut comp = arg.1;
+                if comp.ends_with("{") {
+
+                }
+            
+            } else if in_vec { 
+                if line.contains("=") {
+                    let arg = line.split_once("=").unwrap();
+                    if !arg.1.contains("=") {
+                        //println!("after equal:{}", arg.1);
+                        //perform_operation(arg.1.into(), &mut var_vec, i);// variable is mutated by function
+                        let raw_arg = arg.1.replace(" ", "");
+                        let variable_value_string = parse(&replace_varname_in_string(arg.1, &var_vec));
+                        let variable_value: VariableType;
+                        if variable_value_string.contains(".") {
+                            variable_value = VariableType::Float(variable_value_string.parse().unwrap());
+                        } else if raw_arg == "true" || raw_arg == "false" {
+                            variable_value =
+                                if variable_value_string == "true" {
+                                    VariableType::Bool(true)
+                                } else {
+                                    VariableType::Bool(false)
+                                };
+                        } else {
+                            //println!("assigning int");
+                            variable_value = VariableType::Integer(variable_value_string.parse().unwrap());      
+                        }
+                        var_vec[i].mutate(variable_value);
+
+                    } else {
+                        println!("incorrect assignment only one = allowed");
+                        return;
+                    }
+                }
+            } else if line.starts_with("//") {
+    
+            } else {
+                println!("Error on line:{} Couldnt parse:{}.\nExiting", line_number, line,);
+                return;
+            }
+        }
+    }
 }
