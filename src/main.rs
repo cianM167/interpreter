1f75b968d90fd42219169d25f31e0c54b8640e53
cianm167 <cianm167@gmail.com>
2025-12-14 23:42:51 +0000

    Changed variable table to use hashmaps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::time::Duration;
 use std::{env, thread::sleep};
 use std::fs::read_to_string;
@@ -14,7 +15,6 @@ enum VariableType {
 }
 
 struct Variable {
-    name: String,
     val: VariableType,
 }
 
@@ -185,55 +185,6 @@ fn first_token(value: &str) -> &str { //returns substring
     return ""; //error out
 }
 
-fn is_in_vec(vec: &Vec<Variable>, varname: String) -> bool {
-    for variable in vec {
-        if variable.name == varname {
-            return true;
-        }
-    }
-    return false;
-}
-
-fn is_in_vec_tup(vec: &Vec<Variable>, varname: String) -> (usize, bool) {
-    for (i, variable) in vec.iter().enumerate() {
-        if variable.name == varname {
-            return (i, true);
-        }
-    }
-    return (0, false);
-}
-
-fn replace_varname_in_string(value: &str, var_vec: &Vec<Variable>) -> String {
-    let mut string = value.to_string();
-    for variable in  var_vec {
-
-        match &variable.val {
-            VariableType::Integer(variable_type) => {
-                if value.contains(&variable.name) {
-                    string = string.replace(&variable.name, &variable_type.to_string());
-                }
-            }
-            VariableType::Float(variable_type) => {
-                if value.contains(&variable.name) {
-                    string = string.replace(&variable.name, &variable_type.to_string());
-                }
-            }
-            VariableType::Bool(variable_type) => {
-                if value.contains(&variable.name) {
-                    string = string.replace(&variable.name, &variable_type.to_string());
-                }
-            }
-            VariableType::String(variable_type) => {
-                if value.contains(&variable.name) {
-                    string = string.replace(&variable.name, &variable_type.to_string());
-                }
-            }
-        }
-    }
-
-    return string;
-}
-
 fn print(tokens: Vec<TokenType>) {
     for token in tokens {
         match token {
@@ -262,10 +213,11 @@ fn main() {
     //let variable_name = fs::read_to_string(args[1].clone()).unwrap();
 
     let file_vec = read_lines(&(args[1]));
-    let tokens = lexer(file_vec.clone());//this is really really slow just here so it compiles
+    let tokens = lexer(file_vec);//this is really really slow just here so it compiles
     println!("{:?}", tokens);
     println!("{:?}", parse(&tokens));
     //println!("{:?}", parse(&tokens[..]));
+    let mut variables: HashMap<&str, Variable> = HashMap::new();
 
     let mut token_iter = tokens.into_iter().peekable();
     while let Some(token) = token_iter.next() {
